ac06aee5ced218e49f2431a732722b63469d1122
Kaveesha Baddage <kaveeshab@millenniumitesp.com>
2018-01-18 17:49:09 +0530

    Create LineChart components to view temperature and humidity readings

--- src/components/widgets/Chart.js
+++ src/components/widgets/Chart.js
@@ -1,29 +1,29 @@
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, AxisLabel, Label } from "recharts"
 import React, { Component } from 'react';
-import { LineChart, CartesianGrid,XAxis, YAxis,Tooltip,Legend,Line} from 'recharts';
 
-const data = [
-    { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
-    { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
-    { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
-    { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
-    { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
-    { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
-    { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
-];
+
 
 class Chart extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+
+
     render() {
         return (
-
-            <LineChart width={730} height={250} data={data}
-                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+            <LineChart width={600} height={300} data={this.props.data}>
+                <XAxis dataKey="name" padding={{ left: 30, right: 30 }} label={{ value: this.props.xAxisLabel, position: "insideBottomRight", dy: 10, stroke: '#666666' }} />
+                <YAxis label={{ value: this.props.yAxisLabel, angle: -90, dy: 10, stroke: '#666666' }} />
                 <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
                 <Tooltip />
                 <Legend />
-                <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                {this.props.sensorData && this.props.sensorData.map((sensor) => (
+                    <Line key={sensor.name} type="monotone" dataKey={sensor.name} stroke={sensor.stroke} />
+                ))}
+
+
             </LineChart>
 
         );
